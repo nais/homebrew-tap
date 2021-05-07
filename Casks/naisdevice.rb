721d2fa6da523efaf2c5cf6e12a61ca13bfafeef
J-K. Solbakken <jksolbakken@gmail.com>
2021-05-07 23:42:03 +0200

    no need for pr handling

--- Casks/naisdevice.rb
+++ Casks/naisdevice.rb
@@ -2,9 +2,9 @@ cask "naisdevice" do
   version "2021-05-06-200955"
   sha256 "d69c98bf65065f250392e1637582704c3f70c97d0fb0fde8d5123874eca6aaba"
 
-  url "https://github.com/nais/device/releases/download/2021-05-06-200955/naisdevice.pkg"
+  url "https://github.com/nais/device/releases/download/#{version}/naisdevice.pkg"
   name "naisdevice"
-  desc "Enabling NAVs developers to connect to internal resources in a secure and friendly manner."
+  desc "Enabling NAVs devs to connect to internal stuff in a secure and friendly manner"
   homepage "https://github.com/nais/device"
 
   pkg "naisdevice.pkg"
