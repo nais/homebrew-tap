63f0690fbe2d1aebfd3a39b47872d9de2ef82fc7
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-27 09:55:45 +0200

    Brew formula update for nais-cli version 1.20210927075024

--- Formula/nais.rb
+++ Formula/nais.rb
@@ -5,24 +5,24 @@
 class Nais < Formula
   desc "Simple Aiven Kafka Debug Cli"
   homepage "https://github.com/nais/nais-cli"
-  version "1.20210927072718"
+  version "1.20210927075024"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/nais/nais-cli/releases/download/1.20210927072718/nais-cli_1.20210927072718_darwin_amd64.tar.gz"
-      sha256 "32284eabe768976d26aef0631abae6fd8a9477a0d950c2d74736d088780235f3"
+      url "https://github.com/nais/nais-cli/releases/download/1.20210927075024/nais-cli_1.20210927075024_darwin_amd64.tar.gz"
+      sha256 "3e1e32ef80d628ec2b5f84ef0f4cc1b106a44f9937e44959a61ba7da808e9d7c"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/nais/nais-cli/releases/download/1.20210927072718/nais-cli_1.20210927072718_darwin_arm64.tar.gz"
-      sha256 "e3c4518a1d020cdad6f8099c8c90eec9a98472d19d2c26955d5f84b3dd153140"
+      url "https://github.com/nais/nais-cli/releases/download/1.20210927075024/nais-cli_1.20210927075024_darwin_arm64.tar.gz"
+      sha256 "5e96840ed909f3be69e42f885a3985947ab2450f4352fb71bbdc222be652050b"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/nais/nais-cli/releases/download/1.20210927072718/nais-cli_1.20210927072718_linux_amd64.tar.gz"
-      sha256 "e29aab4c77958e903238354061d42281c67fc93cde4d51f8794c2c7065469032"
+      url "https://github.com/nais/nais-cli/releases/download/1.20210927075024/nais-cli_1.20210927075024_linux_amd64.tar.gz"
+      sha256 "b7a61da33bf088d0fb6810550e875064ea0d9057482b36e4e6f1e7fc38474275"
     end
   end
 
