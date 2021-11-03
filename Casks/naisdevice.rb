a2bb18bf289c23a670fb11d3140c39a403a14551
naisdevice pipeline <aura@nav.no>
2021-11-03 12:44:38 +0000

    Bump naisdevice.pkg version to 2021-11-03-123859

--- Casks/naisdevice.rb
+++ Casks/naisdevice.rb
@@ -1,6 +1,6 @@
 cask "naisdevice" do
-  version "2021-10-27-123904"
-  sha256 "e539832783a86b9f73eaf2d29ae8eeb4570a840915cd666d76300c7fb2c6d008"
+  version "2021-11-03-123859"
+  sha256 "ad6038b3d244e142a7e4fc4997f58c807947b1458469027bde07390b698e56f8"
 
   url "https://github.com/nais/device/releases/download/#{version}/naisdevice.pkg"
   name "naisdevice"
