52a52f0da3a4f53b08e29ae26b278cb83b8b09f6
goreleaserbot <goreleaser@carlosbecker.com>
2022-04-28 12:35:38 +0000

    Brew formula update for nais-salsa version v0.1.20220428123123

--- Formula/salsa.rb
+++ Formula/salsa.rb
@@ -5,21 +5,21 @@
 class Salsa < Formula
   desc "Command-line interface for SLSA provenance"
   homepage "https://github.com/nais/salsa"
-  version "0.1.20220428122434"
+  version "0.1.20220428123123"
   license "MIT"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/nais/salsa/releases/download/v0.1.20220428122434/nais-salsa_0.1.20220428122434_darwin_arm64.tar.gz"
-      sha256 "83c3ba6c13fc1538b3194ca48b624a1e53ada9c467cd665bee158761e908df12"
+    if Hardware::CPU.intel?
+      url "https://github.com/nais/salsa/releases/download/v0.1.20220428123123/nais-salsa_0.1.20220428123123_darwin_amd64.tar.gz"
+      sha256 "a048c13802965bdf150249a96b3d45a9ba54e23a88b7aefbb816fa60abdd4073"
 
       def install
         bin.install "salsa"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/nais/salsa/releases/download/v0.1.20220428122434/nais-salsa_0.1.20220428122434_darwin_amd64.tar.gz"
-      sha256 "b09147fb9168d1ffbd492d53aad51a90297c7ea13f94cd3892b4671b3eee3958"
+    if Hardware::CPU.arm?
+      url "https://github.com/nais/salsa/releases/download/v0.1.20220428123123/nais-salsa_0.1.20220428123123_darwin_arm64.tar.gz"
+      sha256 "8bcbce3fe9b4a4a85c2ee149a83dd068d66de348c30bf0646a2eb85bdc0d0223"
 
       def install
         bin.install "salsa"
@@ -29,8 +29,8 @@ class Salsa < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/nais/salsa/releases/download/v0.1.20220428122434/nais-salsa_0.1.20220428122434_linux_amd64.tar.gz"
-      sha256 "dfbc07e7eeae894a4dd82fff504dc98c405bcddd70be315c72aa1066c1dc89ad"
+      url "https://github.com/nais/salsa/releases/download/v0.1.20220428123123/nais-salsa_0.1.20220428123123_linux_amd64.tar.gz"
+      sha256 "a7f54c4d8c55a046f52ccbbca7b29ac533ad3d3551042bca991cf0d21ae25e7d"
 
       def install
         bin.install "salsa"
