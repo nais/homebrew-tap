68fed693963b540fed22fb648bdc60eee0acf074
naisdevice pipeline <aura@nav.no>
2022-10-18 10:30:35 +0000

    Bump naisdevice.pkg version to 2022-10-18-122452

--- Casks/naisdevice-tenant.rb
+++ Casks/naisdevice-tenant.rb
@@ -1,5 +1,5 @@
 cask "naisdevice-tenant" do
-  version "2022-10-17-110535"
+  version "2022-10-18-122452"
   sha256 ""
 
   url "https://github.com/nais/device/releases/download/#{version}/naisdevice-tenant.pkg"
