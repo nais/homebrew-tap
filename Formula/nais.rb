5caae69764ff08a7363a1e6dd24aa6b8548b8d8a
goreleaserbot <goreleaser@carlosbecker.com>
2023-06-09 07:36:04 +0000

    Brew formula update for nais-cli version 1.20230609072806

--- Formula/nais.rb
+++ Formula/nais.rb
@@ -5,20 +5,20 @@
 class Nais < Formula
   desc "Command-line interface for NAIS"
   homepage "https://github.com/nais/cli"
-  version "1.20230607124838"
+  version "1.20230609072806"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/nais/cli/releases/download/1.20230607124838/nais-cli_1.20230607124838_darwin_amd64.tar.gz"
-      sha256 "497404ebfd6c761e524ac067316436db65e887838705e61ba71d94ff8581bf55"
+    if Hardware::CPU.arm?
+      url "https://github.com/nais/cli/releases/download/1.20230609072806/nais-cli_1.20230609072806_darwin_arm64.tar.gz"
+      sha256 "79eb3b4f1b391ead4f057a36c3deb098b16bf8b8f8f9fa854b63e2500b6ad618"
 
       def install
         bin.install "nais"
       end
     end
-    if Hardware::CPU.arm?
-      url "https://github.com/nais/cli/releases/download/1.20230607124838/nais-cli_1.20230607124838_darwin_arm64.tar.gz"
-      sha256 "e053c18b7a06ff52d4898c0f6f36a3029c4838aedc6fa891ea1d1a3b75b2339e"
+    if Hardware::CPU.intel?
+      url "https://github.com/nais/cli/releases/download/1.20230609072806/nais-cli_1.20230609072806_darwin_amd64.tar.gz"
+      sha256 "faee83baaa80d6e05f70da2d790fbd7daf912e79c15dab18d8860b0ae4cdbb5e"
 
       def install
         bin.install "nais"
@@ -28,8 +28,8 @@ class Nais < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/nais/cli/releases/download/1.20230607124838/nais-cli_1.20230607124838_linux_amd64.tar.gz"
-      sha256 "096a72a4e6086347a939c6a44b9bc10f2b6fd32cbf41833fe32e000002ef57a9"
+      url "https://github.com/nais/cli/releases/download/1.20230609072806/nais-cli_1.20230609072806_linux_amd64.tar.gz"
+      sha256 "8e13eb8c6d8a585d19c16317bc4d0dd52962d8202ca1568a4d5f784d35a787e6"
 
       def install
         bin.install "nais"
