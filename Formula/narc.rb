aca6729275f17fa019f2b70a425d62c9263b966f
goreleaserbot <goreleaser@carlosbecker.com>
2023-06-30 08:12:42 +0000

    Brew formula update for narc-cli version 1.20230630081012

--- Formula/narc.rb
+++ Formula/narc.rb
@@ -5,20 +5,20 @@
 class Narc < Formula
   desc "NAIS Administration CLI"
   homepage "https://github.com/nais/narcos"
-  version "1.20230630072434"
+  version "1.20230630081012"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/nais/narcos/releases/download/1.20230630072434/narc-cli_1.20230630072434_darwin_arm64.tar.gz"
-      sha256 "b03b074e0b7f902c233ce4c60c406508c57b98e2f944f3ebb58ea4a664ecb2c4"
+      url "https://github.com/nais/narcos/releases/download/1.20230630081012/narc-cli_1.20230630081012_darwin_arm64.tar.gz"
+      sha256 "1ba4166cef57f9a3f321a0bf3a904722baf7f96a42004eef9ba8372eecb2d0ce"
 
       def install
         bin.install "narc"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/nais/narcos/releases/download/1.20230630072434/narc-cli_1.20230630072434_darwin_amd64.tar.gz"
-      sha256 "1f138b8f4cc01f20e13f58209fe869ed93af04fc2b7b1ebb9f3cdc68f37139e3"
+      url "https://github.com/nais/narcos/releases/download/1.20230630081012/narc-cli_1.20230630081012_darwin_amd64.tar.gz"
+      sha256 "4d00e5529b237ffcdb55ce997aa29899818bf42f397106621240601d81b4c692"
 
       def install
         bin.install "narc"
@@ -28,8 +28,8 @@ class Narc < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/nais/narcos/releases/download/1.20230630072434/narc-cli_1.20230630072434_linux_amd64.tar.gz"
-      sha256 "8759aa2633c25aee0ded8e4019c395944d9be68e7b32a7d033c26a56fa2d75a5"
+      url "https://github.com/nais/narcos/releases/download/1.20230630081012/narc-cli_1.20230630081012_linux_amd64.tar.gz"
+      sha256 "b16e7ebdcf7c22a190cab169d244d53c6ca63c7da63a0225f28b18759e9ec74c"
 
       def install
         bin.install "narc"
