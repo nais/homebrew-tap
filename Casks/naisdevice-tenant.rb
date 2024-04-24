9b69281a61f22b67c0adad99319f2c938117de1c
naisdevice pipeline <aura@nav.no>
2024-04-24 13:07:20 +0000

    Bump naisdevice.pkg version to 2024-04-24-150217

--- Casks/naisdevice-tenant.rb
+++ Casks/naisdevice-tenant.rb
@@ -1,6 +1,6 @@
 cask "naisdevice-tenant" do
-  version "2024-04-04-113720"
-  sha256 "6e12f02429be687e1554d0b1ad285ad369a4da9d546e2b7253f0852fd6f4502c"
+  version "2024-04-24-150217"
+  sha256 "da7d751622d53a5ddb0c8ba55299916633da429fbcac8d76312a1b483ca471c2"
 
   url "https://github.com/nais/device/releases/download/#{version}/naisdevice-tenant.pkg"
   name "naisdevice-tenant"
--- Casks/naisdevice.rb
+++ Casks/naisdevice.rb
@@ -1,6 +1,6 @@
 cask "naisdevice" do
-  version "2024-04-04-113720"
-  sha256 "4193e5d32c575556e8ea80d64a07b40ce176a7e99ee9a429ddd8cd35c503c5a9"
+  version "2024-04-24-150217"
+  sha256 "16eaeb1ae6d47f25ac8d879ccf100bcaab483823192d56ecb3119892cb328e74"
 
   url "https://github.com/nais/device/releases/download/#{version}/naisdevice.pkg"
   name "naisdevice"
